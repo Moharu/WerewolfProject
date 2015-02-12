0f4787069eebae10f464cbf006734a930114b491
João Teló <joaotelo.nh@hotmail.com>
2015-02-12 00:45:41 -0200

    Networking Demo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10f;
+	
+	void Update()
+	{
+		if (networkView.isMine) {
+			InputMovement();
+		}
+	}
+	
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+	}
+
+	private void InputColorChange()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+	}
+	
+	[RPC] void ChangeColorTo(Vector3 color)
+	{
+		renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+		
+		if (networkView.isMine)
+			networkView.RPC("ChangeColorTo", RPCMode.OthersBuffered, color);
+	}
+}
