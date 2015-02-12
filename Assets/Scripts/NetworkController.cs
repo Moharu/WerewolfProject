1309e3c716b97eeee26617171f29c6f1027b6865
João Teló <joaotelo.nh@hotmail.com>
2015-02-12 21:17:28 -0200

    First UI version

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -25,26 +25,6 @@ public class NetworkController : MonoBehaviour {
 		MasterServer.RegisterHost(typeName, gameName);
 	}
 
-	void OnGUI()
-	{
-		if (!Network.isClient && !Network.isServer) {
-			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
-				StartServer ();
-			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
-				RefreshHostList ();
-			
-			if (hostList != null) {
-				for (int i = 0; i < hostList.Length; i++) {
-					if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList [i].gameName))
-						JoinServer (hostList [i]);
-				}
-			}
-		} else {
-			GUI.TextArea(new Rect (20,20,67,23), "Players: "+(Network.connections.Length + 1));
-			if (GUI.Button (new Rect(920,410,150,60), "Disconnect")) Network.Disconnect ();
-		} 
-	}
-
 	private HostData[] hostList;
 	
 	private void RefreshHostList()
