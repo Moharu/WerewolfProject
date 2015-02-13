c2be1aeb69cb17a27c45ce61b0a6eae03e7b68f3
João Teló <joaotelo.nh@hotmail.com>
2015-02-12 23:39:38 -0200

    MegaCommit da funcionalidade Multiplayer
    AS COISAS FUNCIONAM!!!!111!1

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -6,7 +6,20 @@ public class NetworkController : MonoBehaviour {
 	private const string typeName = "WerewolfProject";
 	private const string gameName = "RoomName";
 
-	public int playerCount=0;
+	public int playerCount;
+
+	void Update(){
+		if (Network.isServer){
+			playerCount = Network.connections.Length + 1;
+			networkView.RPC ("receivePlayerCount", RPCMode.Others, playerCount);
+		}
+	}
+
+	[RPC]
+	void receivePlayerCount(int count){
+		playerCount = count;
+	}
+
 
 	public Vector3[] playerPositions = new [] { 
 		new Vector3(0f,0.5f,3f), 
@@ -18,6 +31,25 @@ public class NetworkController : MonoBehaviour {
 		new Vector3(-3f,0.5f,0f),
 		new Vector3(-2f,0.5f,2f)
 	};
+
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
+				StartServer ();
+			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList ();
+			if (hostList != null) {
+				for (int i = 0; i < hostList.Length; i++) {
+					if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList [i].gameName))
+						JoinServer (hostList [i]);
+				}
+			}
+		} else {
+			GUI.TextArea(new Rect (20,20,67,23), "Players: "+ playerCount);
+			if (GUI.Button (new Rect(0,50,150,60), "Disconnect")) Network.Disconnect ();
+		}
+	}
 
 	private void StartServer()
 	{
@@ -47,27 +79,30 @@ public class NetworkController : MonoBehaviour {
 	
 	void OnServerInitialized()
 	{
-		SpawnPlayer();
+		SpawnPlayer ();
 	}
 	
 	void OnConnectedToServer()
 	{
-		SpawnPlayer();
+		SpawnPlayer ();
 	}
 	
 	private void SpawnPlayer()
 	{
 		Network.Instantiate(playerPrefab, playerPositions[Network.connections.Length], Quaternion.identity, 0);
+
 	}
 
 	void OnDisconnectedFromServer(NetworkDisconnection info) {
-		foreach(GameObject player in GameObject.FindGameObjectsWithTag("Players")){
+		Network.DestroyPlayerObjects (Network.player);
+		foreach (GameObject player in GameObject.FindGameObjectsWithTag("Players")) {
 			Destroy (player);
 		}
 	}
 
 	void OnPlayerDisconnected(NetworkPlayer player){
-		Network.DestroyPlayerObjects (player);
+		if(Network.isServer) Network.DestroyPlayerObjects (player);
+		Network.RemoveRPCs(player);	//FUNCIONA
 	}
 
 	void OnApplicationQuit(){
